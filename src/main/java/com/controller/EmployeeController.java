dfcaf8b1f1cf434b4187a951d207afc00b390317
BecaWang <w121093@hotmail.com.tw>
2019-11-11 22:43:14 +0800

    updateController
    整理一下基本post語法

--- src/main/java/com/controller/EmployeeController.java
+++ src/main/java/com/controller/EmployeeController.java
@@ -3,6 +3,7 @@ package main.java.com.controller;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import main.java.com.model.Employee;
@@ -10,10 +11,21 @@ import main.java.com.model.EmployeeJsonRespone;
 
 @RestController
 public class EmployeeController {
-	
-	   @PostMapping(value = "/saveEmployee", produces = { MediaType.APPLICATION_JSON_VALUE })
-	   public EmployeeJsonRespone saveEmployee(@ModelAttribute  Employee employee) {
-
+		   
+/**
+ * 	 複習一下rest post
+ * url:http://localhost:8080/saveEmployee
+ *   {
+			"firstName":"wang",
+			"lastName":"ann",
+			"email":"ok@gmail.com",
+			"designation":"taipei"
+		
+	}
+	*/
+	   @PostMapping(value = "/saveEmployee", produces = "application/json")
+	   public EmployeeJsonRespone saveEmployee(@RequestBody  Employee employee) {
+		   
 	      EmployeeJsonRespone respone = new EmployeeJsonRespone();
 	      respone.setEmployee(employee);
 	      return respone;
